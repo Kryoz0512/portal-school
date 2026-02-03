e077576538ac05640f1666b8afa4bfe30409a2e6
Kryoz0512 <markrobertbayudang5@gmail.com>
2026-02-03 12:41:13 +0800

    "fixed sidebar student"

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -28,7 +28,7 @@ export function Sidebar({ items, role, onClose, isOpen = true }) {
     <aside
       className={`
         fixed left-0 top-0 h-screen w-64 bg-sidebar text-sidebar-foreground shadow-lg
-        transition-transform duration-300 ease-in-out z-40
+        transition-transform duration-300 ease-in-out z-50
         md:translate-x-0 md:relative flex flex-col
         ${isOpen ? 'translate-x-0' : '-translate-x-full'}
       `}
